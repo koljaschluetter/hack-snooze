b5e1a159801986144bc25d13c68b05846460f9b2
koljaschluetter <kolja@schluetter.co>
2018-11-14 16:37:37 -0800

    second commit

--- api-classes.js
+++ api-classes.js
@@ -1,12 +1,105 @@
-const BASE_URL = "https://hack-or-snooze-v2.herokuapp.com";
+const BASE_URL = 'https://hack-or-snooze-v2.herokuapp.com';
+
+const USER_TOKEN =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImtvbGphLWdpbiIsImlhdCI6MTU0MjIzMDYwMH0.EiDDAI-pCXgVg-rTsKAaYHaZVAdDGXrouXVhuCo5MII';
 
 class StoryList {
+  constructor(stories) {
+    this.stories = stories;
+  }
+
   static getStories(cb) {
-    // fetch stories from API
-    return cb(/* fetched stories go here */);
+    $.getJSON(`${BASE_URL}/stories`, function(response) {
+      const stories = response.stories.map(function(story) {
+        const { username, title, author, url, storyId } = story;
+
+        return new Story(username, title, author, url, storyId);
+      });
+
+      const storyList = new StoryList(stories);
+      return cb(storyList);
+    });
   }
 }
 
-class User {}
+class User {
+  constructor(username, password, name, loginToken, favorites, ownStories) {
+    this.username = username;
+    this.password = password;
+    this.name = name;
+    this.loginToken = loginToken;
+    this.favorites = favorites;
+    this.ownStories = ownStories;
+  }
 
-class Story {}
+  static create(username, password, name, cb) {
+    let userObj = {
+      user: {
+        name,
+        username,
+        password
+      }
+    };
+
+    $.post(
+      `https://hack-or-snooze-v2.herokuapp.com/signup`,
+      userObj,
+      response => {
+        const { username, name, favorites, stories } = response.user;
+        const token = response.token;
+
+        let user = new User(
+          username,
+          password,
+          name,
+          token,
+          favorites,
+          stories
+        );
+
+        return cb(user);
+      }
+    );
+  }
+
+  login(cb) {
+    let loginObj = {
+      user: {
+        username: this.username,
+        password: this.password
+      }
+    };
+
+    console.log(this.username, this.password);
+
+    $.post(`https://hack-or-snooze-v2.herokuapp.com/login`, loginObj, function(
+      response
+    ) {
+      return cb(response);
+    });
+  }
+
+  retrieveDetails(cb) {
+    $.get(
+      `https://hack-or-snooze-v2.herokuapp.com/users/${this.username}?token=${
+        this.loginToken
+      }`,
+      response => {
+        this.favorites = response.user.favorites;
+        this.ownStories = response.user.stories;
+
+        return cb(this);
+      }
+    );
+  }
+}
+
+class Story {
+  constructor(author, title, url, username, storyId) {
+    this.author = author;
+    this.title = title;
+    this.url = url;
+    this.username = username;
+    this.storyId = storyId;
+  }
+}
